df56c729463b9842d7b4e3a1a1d07e587571a5e6
Nicolas Rodriguez <nicolas.rodriguezl@hotmail.com>
2022-01-27 20:08:22 -0500

    inclusión parte2

--- script_complementaria1.R
+++ script_complementaria1.R
@@ -1,6 +1,6 @@
 ####PARTE 1
 
-
+library(tidyverse)
 
 ## cargar datos 
 data("uspop")
@@ -16,3 +16,16 @@ uspop
 uspop[5]
 uspop[c(5,8)]
 uspop[c(5:8)]
+
+
+
+###PARTE 2
+
+
+#ejercicio
+
+x<-c(2,4,6,8,10)
+y<-x[c(2,4)]
+
+data("iris")
+
